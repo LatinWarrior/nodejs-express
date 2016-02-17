069bb8cf22841a927bdb658d730337a6042622eb
Luis <luis.f.blanco@gmail.com>
2016-02-16 19:01:24 -0600

    Fixed issue with second page not loading correctly

--- app.js
+++ app.js
@@ -29,23 +29,17 @@ app.set('view engine', 'ejs');
 
 app.use('/Books', bookRouter);
 
-app.get('/', function (req, res) {
-    //res.send('Hello World!');
-    res.render('index', {
-        title: 'Hello from render',
-        nav: [{
-            Link: '/Books',
-            Text: 'Books'
-        }, {
-            Link: '/Authors',
-            Text: 'Authors'
-        }]
-    });
-});
-
-app.get('/books', function (req, res) {
-    res.send('Hello Books!');
-});
+//app.get('/', function (req, res) {
+//    //res.send('Hello World!');
+//    res.render('index', {
+//        title: 'Hello from render',
+//        nav: nav
+//    });
+//});
+//
+//app.get('/books', function (req, res) {
+//    res.send('Hello Books!');
+//});
 
 app.listen(port, function (err) {
     console.log('running server on port ' + port);
